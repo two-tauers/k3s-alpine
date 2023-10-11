45d414a6fa3c49b146e32bf3c2a4c581812d9104
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2023-10-11 17:54:21 +0100

    issue/7 get k3s config from node config directly

--- scripts/build-overlay.sh
+++ scripts/build-overlay.sh
@@ -16,11 +16,6 @@ which yq > /dev/null 2>&1 || log "ERROR" "Package yq is not installed"
 
 log "INFO" "Reading config file"
 hostname=$(yq '.hostname' < $CONFIG) || log "ERROR" "Could not read hostname from the config"
-k3s_exec=$(yq '.k3s.exec' < $CONFIG) || log "ERROR" "Could not read k3s exec mode from the config"
-
-if [ "$k3s_exec" != "server" ] && [ "$k3s_exec" != "agent" ]; then
-    log "ERROR" "Unknown k3s exec mode: '$k3s_exec'. Must be one of: 'server', 'agent'"
-fi
 
 log "INFO" "Generating host keys"
 mkdir -p overlay/etc/ssh/
@@ -38,22 +33,13 @@ cp -r overlay/* $STAGING
 log "INFO" "Setting a hostname"
 echo $hostname > $STAGING/etc/hostname
 
-
-
 log "INFO" "Adding node config"
 cp $CONFIG $STAGING/etc/node-config.yaml
-
-
-log "INFO" "Adding k3s config"
-mkdir -p $STAGING/etc/boot-config/k3s
-echo $CONFIG > $STAGING/etc/boot-config/k3s/config.yaml
-echo $k3s_exec > $STAGING/etc/boot-config/k3s/exec
 
 log "INFO" "Adding k3s installer"
 mkdir -p $STAGING/etc/boot-data/install
 cp bin/k3s-install.sh $STAGING/etc/boot-data/install/k3s-install.sh
 cp bin/k3s $STAGING/etc/boot-data/install/k3s
-
 
 log "INFO" "Packaging overlay"
 chmod +x $STAGING/etc/local.d/headless.start
